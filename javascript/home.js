cf60cc3d0fef5d899a9381fd8ea08419be4607e1
unknown <patanchev23@codingburgas.bg>
2025-06-08 15:00:59 +0300

    Add js for the button theme switch in home page

--- javascript/home.js
+++ javascript/home.js
@@ -5,3 +5,23 @@ const map = L.map('map').setView([42.7339, 25.4858], 7);
   const cities = [
     { name: "Sofia", coords: [42.6977, 23.3219], url: "sofia.html" }
   ];
+
+   let darkmode = localStorage.getItem('darkmode')
+  const themeSwitch = document.getElementById('theme-switch')
+
+const enableDarkmode = () => {
+  document.body.classList.add('darkmode')
+  localStorage.setItem('darkmode', 'active')
+}
+
+const disableDarkmode = () => {
+    document.body.classList.remove('darkmode')
+  localStorage.setItem('darkmode', null)
+}
+
+if(darkmode === "active") enableDarkmode()
+
+themeSwitch.addEventListener("click", () => {
+  darkmode = localStorage.getItem('darkmode')
+  darkmode != "active" ? enableDarkmode() : disableDarkmode()
+})
